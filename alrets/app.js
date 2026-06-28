70a1cb9a4a0c2dd1e1098657623b702258d2a1c5
Usama khan <usamakhan@digitalgravity.ae>
2026-06-28 11:37:01 +0500

    alert and var

--- alrets/app.js
+++ alrets/app.js
@@ -1,3 +1,53 @@
-alert("Thankyou For Reaching Out To Us! We Will Get Back To You Soon!");
 
-console.log("Form Submitted Successfully!");
+// Alerts are a simple way to provide feedback to users when they interact with a web page. In this case, the alert is used to thank the user for reaching out and to inform them that they will receive a response soon. The console.log statement is used to log a message in the browser's console, which can be helpful for debugging purposes.
+
+// To use this code, you would typically include it in a JavaScript file that is linked to your HTML page. When the user submits a form or clicks a button that triggers this code, they will see the alert message and the console will log the success message.
+
+
+// alert("aj hamari js ki class hia") //alert ek function hai hia baan banaya phele se hi 
+
+
+
+// // Variable can store data store on a container its use on when you needed 
+
+// var school = "Digital Gravity"
+// var campus = "NASTP"
+// var domain = "Frontend"
+// var batch = 20
+// var nationalty;
+// nationalty = "U.A.E";
+
+
+
+
+// var cssResult = "Pass"
+
+// var cssResult;    // varibale declear / //Declearation
+
+// ccsResult = "Fail";  // variable initailzation 
+
+// console.log(ccsResult);
+
+
+alert("alerts alrets")
+
+var school = "smit"
+var batch = 18
+var domian  = "Digital Gravity"
+
+var nationalty;
+nationalty = "pakistani"; 
+
+console.log(" Usama is " + nationalty + " Nationalty");
+
+var nationaly ;
+nationalty = "USAMA KHAN"
+
+
+console.log(nationalty);
+
+
+
+
+
+
